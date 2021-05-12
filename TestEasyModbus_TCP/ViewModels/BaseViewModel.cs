6912c1f59c54ae60cb480a518a0b68765d2ad74c
petrov-aa78 <petrovandrey7@gmail.com>
2021-05-12 08:58:45 +0400

    первое сохранение

--- TestEasyModbus_TCP/ViewModels/BaseViewModel.cs
+++ TestEasyModbus_TCP/ViewModels/BaseViewModel.cs
@@ -13,24 +13,112 @@ namespace TestEasyModbus_TCP.ViewModels
 
     public class BaseViewModel
     {
-      
+        public string ErrorRead { set;  get; }
 
-        public string DataReader1 { set; get; }
-        public string DataReader2 { set; get; }
-        public string DataReader3 { set; get; }
+        public string DataReadQF1_PSUM3_W { get; }
+        public string DataReadQS1_PSUM3_W { get; }
+        public string DataReadQS3_PSUM3_W { get; }
+        public string DataReadQS4_PSUM3_W { get; }
+        public string DataReadQS5_PSUM3_W { get; }
+        public string DataReadQS6_PSUM3_W { get; }
+        public string DataReadQS7_PSUM3_W { get; }
+        public string DataReadQS8_PSUM3_W { get; }
+        public string DataReadQS9_PSUM3_W { get; }
+        public string DataReadQS10_PSUM3_W { get; }
+        public string DataReadQS11_PSUM3_W { get; }
+        public string DataReadQS12_PSUM3_W { get; }
+        public string DataReadQS13_PSUM3_W { get; }
+        public string DataReadQS14_PSUM3_W { get; }
+        public string DataReadQS15_PSUM3_W { get; }
+        public string DataReadQS16_PSUM3_W { get; }
+        public string DataReadQS17_PSUM3_W { get; }
+        public string DataReadQS18_PSUM3_W { get; }
+        public string DataReadQS19_PSUM3_W { get; }
+        public string DataReadQF3_PSUM3_W { get; }
+        public string DataReadQF4_PSUM3_W { get; }
+        public string DataReadQF5_PSUM3_W { get; }
+        public string DataReadQF6_PSUM3_W { get; }
+        public string DataReadQF7_PSUM3_W { get; }
+        public string DataReadQF8_PSUM3_W { get; }
 
-        public string DataReader4 { set; get; }
-        public string DataReader5 { set; get; }
-        public string DataReader6 { set; get; }
+        public string DataReadQF1_WHSUM3_Wh { get; }
+        public string DataReadQS1_WHSUM3_Wh { get; }
+        public string DataReadQS3_WHSUM3_Wh { get; }
+        public string DataReadQS4_WHSUM3_Wh { get; }
+        public string DataReadQS5_WHSUM3_Wh { get; }
+        public string DataReadQS6_WHSUM3_Wh { get; }
+        public string DataReadQS7_WHSUM3_Wh { get; }
+        public string DataReadQS8_WHSUM3_Wh { get; }
+        public string DataReadQS9_WHSUM3_Wh { get; }
+        public string DataReadQS10_WHSUM3_Wh { get; }
+        public string DataReadQS11_WHSUM3_Wh { get; }
+        public string DataReadQS12_WHSUM3_Wh { get; }
+        public string DataReadQS13_WHSUM3_Wh { get; }
+        public string DataReadQS14_WHSUM3_Wh { get; }
+        public string DataReadQS15_WHSUM3_Wh { get; }
+        public string DataReadQS16_WHSUM3_Wh { get; }
+        public string DataReadQS17_WHSUM3_Wh { get; }
+        public string DataReadQS18_WHSUM3_Wh { get; }
+        public string DataReadQS19_WHSUM3_Wh { get; }
+        public string DataReadQF3_WHSUM3_Wh { get; }
+        public string DataReadQF4_WHSUM3_Wh { get; }
+        public string DataReadQF5_WHSUM3_Wh { get; }
+        public string DataReadQF6_WHSUM3_Wh { get; }
+        public string DataReadQF7_WHSUM3_Wh { get; }
+        public string DataReadQF8_WHSUM3_Wh { get; }
 
-        public string DataReader7 { set; get; }
-        public string DataReader8 { set; get; }
-        public string DataReader9 { set; get; }
+        public string DataReadQF2_PSUM3_W { get; }
+        public string DataReadQS20_PSUM3_W { get; }
+        public string DataReadQS21_PSUM3_W { get; }
+        public string DataReadQS22_PSUM3_W { get; }
+        public string DataReadQS23_PSUM3_W { get; }
+        public string DataReadQS24_PSUM3_W { get; }
+        public string DataReadQS25_PSUM3_W { get; }
+        public string DataReadQS26_PSUM3_W { get; }
+        public string DataReadQS27_PSUM3_W { get; }
+        public string DataReadQS28_PSUM3_W { get; }
+        public string DataReadQS29_PSUM3_W { get; }
+        public string DataReadQS31_PSUM3_W { get; }
+        public string DataReadQS32_PSUM3_W { get; }
+        public string DataReadQS33_PSUM3_W { get; }
+        public string DataReadQS34_PSUM3_W { get; }
+        public string DataReadQS35_PSUM3_W { get; }
+        public string DataReadQS36_PSUM3_W { get; }
+        public string DataReadQS37_PSUM3_W { get; }
+        public string DataReadQS38_PSUM3_W { get; }
+        public string DataReadQF9_PSUM3_W { get; }
+        public string DataReadQF10_PSUM3_W { get; }
+        public string DataReadQF11_PSUM3_W { get; }
+        public string DataReadQF12_PSUM3_W { get; }
+        public string DataReadQF13_PSUM3_W { get; }
 
-        private string U = " В";
-        private string U1 = "LN1 - ";
-        private string U2 = "LN2 - ";
-        private string U3 = "LN3 - ";
+        public string DataReadQF2_WHSUM3_Wh { get; }
+        public string DataReadQS20_WHSUM3_Wh { get; }
+        public string DataReadQS21_WHSUM3_Wh { get; }
+        public string DataReadQS22_WHSUM3_Wh { get; }
+        public string DataReadQS23_WHSUM3_Wh { get; }
+        public string DataReadQS24_WHSUM3_Wh { get; }
+        public string DataReadQS25_WHSUM3_Wh { get; }
+        public string DataReadQS26_WHSUM3_Wh { get; }
+        public string DataReadQS27_WHSUM3_Wh { get; }
+        public string DataReadQS28_WHSUM3_Wh { get; }
+        public string DataReadQS29_WHSUM3_Wh { get; }
+        public string DataReadQS31_WHSUM3_Wh { get; }
+        public string DataReadQS32_WHSUM3_Wh { get; }
+        public string DataReadQS33_WHSUM3_Wh { get; }
+        public string DataReadQS34_WHSUM3_Wh { get; }
+        public string DataReadQS35_WHSUM3_Wh { get; }
+        public string DataReadQS36_WHSUM3_Wh { get; }
+        public string DataReadQS37_WHSUM3_Wh { get; }
+        public string DataReadQS38_WHSUM3_Wh { get; }
+        public string DataReadQF9_WHSUM3_Wh { get; }
+        public string DataReadQF10_WHSUM3_Wh { get; }
+        public string DataReadQF11_WHSUM3_Wh { get; }
+        public string DataReadQF12_WHSUM3_Wh { get; }
+        public string DataReadQF13_WHSUM3_Wh { get; }
+
+
+
 
 
         #region Секция шин 1 - Инициализация
@@ -150,6 +238,7 @@ namespace TestEasyModbus_TCP.ViewModels
         static RegMesUMG QF12Data = new RegMesUMG { };
         static RegMesUMG QF13Data = new RegMesUMG { };
 
+       
 
         public BaseViewModel()
         {
@@ -164,28 +253,28 @@ namespace TestEasyModbus_TCP.ViewModels
             QS7Data  = QS7.GetAllData(QS7Data );
             QS8Data  = QS8.GetAllData(QS8Data );
             QS9Data  = QS9.GetAllData(QS9Data );
-            QS10Data = QS1.GetAllData(QS10Data);
-            QS11Data = QS1.GetAllData(QS11Data);
-            QS12Data = QS1.GetAllData(QS12Data);
-            QS13Data = QS1.GetAllData(QS13Data);
-            QS14Data = QS1.GetAllData(QS14Data);
-            QS15Data = QS1.GetAllData(QS15Data);
-            QS16Data = QS1.GetAllData(QS16Data);
-            QS17Data = QS1.GetAllData(QS17Data);
-            QS18Data = QS1.GetAllData(QS18Data);
-            QS19Data = QS1.GetAllData(QS19Data);
+            QS10Data = QS10.GetAllData(QS10Data);
+            QS11Data = QS11.GetAllData(QS11Data);
+            QS12Data = QS12.GetAllData(QS12Data);
+            QS13Data = QS13.GetAllData(QS13Data);
+            QS14Data = QS14.GetAllData(QS14Data);
+            QS15Data = QS15.GetAllData(QS15Data);
+            QS16Data = QS16.GetAllData(QS16Data);
+            QS17Data = QS17.GetAllData(QS17Data);
+            QS18Data = QS18.GetAllData(QS18Data);
+            QS19Data = QS19.GetAllData(QS19Data);
             QF3Data  = QF3.GetAllData(QF3Data );
             QF4Data  = QF4.GetAllData(QF4Data );
             QF5Data  = QF5.GetAllData(QF5Data );
             QF6Data  = QF6.GetAllData(QF6Data );
             QF7Data =  QF7.GetAllData(QF7Data );
-            QF8Data = QF8.GetAllData (QF8Data);
+            QF8Data =  QF8.GetAllData (QF8Data);
 
             #endregion
 
             #region Сукция 2  - Чтение данных
 
-             QF2Data = QF2.GetAllData(QF2Data); 
+             QF2Data =  QF2.GetAllData(QF2Data); 
              QS20Data = QS20.GetAllData(QS20Data);
              QS21Data = QS21.GetAllData(QS21Data);
              QS22Data = QS22.GetAllData(QS22Data);
@@ -204,7 +293,7 @@ namespace TestEasyModbus_TCP.ViewModels
              QS36Data = QS36.GetAllData(QS36Data);
              QS37Data = QS37.GetAllData(QS37Data);
              QS38Data = QS38.GetAllData(QS38Data);
-             QF9Data = QF9.GetAllData(QF9Data);  
+             QF9Data =  QF9.GetAllData(QF9Data);  
              QF10Data = QF10.GetAllData(QF10Data);
              QF11Data = QF11.GetAllData(QF11Data);
              QF12Data = QF12.GetAllData(QF12Data);
@@ -216,33 +305,123 @@ namespace TestEasyModbus_TCP.ViewModels
 
             try
             {
-               
-                              
-                
-               
+                #region Секция шин 1 - Чтение
+
+                DataReadQF1_PSUM3_W  = $"{( QF1Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS1_PSUM3_W  = $"{( QS1Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS3_PSUM3_W  = $"{( QS3Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS4_PSUM3_W  = $"{( QS4Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS5_PSUM3_W  = $"{( QS5Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS6_PSUM3_W  = $"{( QS6Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS7_PSUM3_W  = $"{( QS7Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS8_PSUM3_W  = $"{( QS8Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS9_PSUM3_W  = $"{( QS9Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS10_PSUM3_W = $"{( QS10Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS11_PSUM3_W = $"{( QS11Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS12_PSUM3_W = $"{( QS12Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS13_PSUM3_W = $"{( QS13Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS14_PSUM3_W = $"{( QS14Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS15_PSUM3_W = $"{( QS15Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS16_PSUM3_W = $"{( QS16Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS17_PSUM3_W = $"{( QS17Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS18_PSUM3_W = $"{( QS18Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS19_PSUM3_W = $"{( QS19Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQF3_PSUM3_W  = $"{( QF3Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQF4_PSUM3_W  = $"{( QF4Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQF5_PSUM3_W  = $"{( QF5Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQF6_PSUM3_W  = $"{( QF6Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQF7_PSUM3_W  = $"{( QF7Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQF8_PSUM3_W  = $"{(QF8Data.PSUM3_W / 1000).ToString("f2")}";
+
+                DataReadQF1_WHSUM3_Wh = $"{( QF1Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS1_WHSUM3_Wh = $"{( QS1Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS3_WHSUM3_Wh = $"{( QS3Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS4_WHSUM3_Wh = $"{( QS4Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS5_WHSUM3_Wh = $"{( QS5Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS6_WHSUM3_Wh = $"{( QS6Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS7_WHSUM3_Wh = $"{( QS7Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS8_WHSUM3_Wh = $"{( QS8Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS9_WHSUM3_Wh = $"{( QS9Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS10_WHSUM3_Wh= $"{( QS10Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS11_WHSUM3_Wh= $"{( QS11Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS12_WHSUM3_Wh= $"{( QS12Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS13_WHSUM3_Wh= $"{( QS13Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS14_WHSUM3_Wh= $"{( QS14Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS15_WHSUM3_Wh= $"{( QS15Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS16_WHSUM3_Wh= $"{( QS16Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS17_WHSUM3_Wh= $"{( QS17Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS18_WHSUM3_Wh= $"{( QS18Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS19_WHSUM3_Wh= $"{( QS19Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQF3_WHSUM3_Wh = $"{( QF3Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQF4_WHSUM3_Wh = $"{( QF4Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQF5_WHSUM3_Wh = $"{( QF5Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQF6_WHSUM3_Wh = $"{( QF6Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQF7_WHSUM3_Wh = $"{( QF7Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQF8_WHSUM3_Wh = $"{( QF8Data.WHSUM3_Wh / 1000).ToString("f0")}";
+
+
+                #endregion
+
+
+
                 #region Секция шин 2 - Чтение
+                DataReadQF2_PSUM3_W  = $"{(QF2Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS20_PSUM3_W = $"{(QS20Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS21_PSUM3_W = $"{(QS21Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS22_PSUM3_W = $"{(QS22Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS23_PSUM3_W = $"{(QS23Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS24_PSUM3_W = $"{(QS24Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS25_PSUM3_W = $"{(QS25Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS26_PSUM3_W = $"{(QS26Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS27_PSUM3_W = $"{(QS27Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS28_PSUM3_W = $"{(QS28Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS29_PSUM3_W = $"{(QS29Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS31_PSUM3_W = $"{(QS31Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS32_PSUM3_W = $"{(QS32Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS33_PSUM3_W = $"{(QS33Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS34_PSUM3_W = $"{(QS34Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS35_PSUM3_W = $"{(QS35Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS36_PSUM3_W = $"{(QS36Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS37_PSUM3_W = $"{(QS37Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQS38_PSUM3_W = $"{(QS38Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQF9_PSUM3_W  = $"{(QF9Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQF10_PSUM3_W = $"{(QF10Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQF11_PSUM3_W = $"{(QF11Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQF12_PSUM3_W = $"{(QF12Data.PSUM3_W / 1000).ToString("f2")}";
+                DataReadQF13_PSUM3_W = $"{(QF13Data.PSUM3_W / 1000).ToString("f2")}";
 
-                              
-                DataReader1 = $"QF2 I_LN1 = { QF2Data.ILN1_A.ToString("#.00")}A";
-                DataReader2 = $"QF2 I_LN2 = { QF2Data.ILN2_A.ToString("#.00")}A";
-                DataReader3 = $"QF2 I_LN3 = { QF2Data.ILN2_A.ToString("#.00")}A";
+                DataReadQF2_WHSUM3_Wh  = $"{(QF2Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS20_WHSUM3_Wh = $"{(QS20Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS21_WHSUM3_Wh = $"{(QS21Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS22_WHSUM3_Wh = $"{(QS22Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS23_WHSUM3_Wh = $"{(QS23Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS24_WHSUM3_Wh = $"{(QS24Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS25_WHSUM3_Wh = $"{(QS25Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS26_WHSUM3_Wh = $"{(QS26Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS27_WHSUM3_Wh = $"{(QS27Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS28_WHSUM3_Wh = $"{(QS28Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS29_WHSUM3_Wh = $"{(QS29Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS31_WHSUM3_Wh = $"{(QS31Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS32_WHSUM3_Wh = $"{(QS32Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS33_WHSUM3_Wh = $"{(QS33Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS34_WHSUM3_Wh = $"{(QS34Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS35_WHSUM3_Wh = $"{(QS35Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS36_WHSUM3_Wh = $"{(QS36Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS37_WHSUM3_Wh = $"{(QS37Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQS38_WHSUM3_Wh = $"{(QS38Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQF9_WHSUM3_Wh  = $"{(QF9Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQF10_WHSUM3_Wh = $"{(QF10Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQF11_WHSUM3_Wh = $"{(QF11Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQF12_WHSUM3_Wh = $"{(QF12Data.WHSUM3_Wh / 1000).ToString("f0")}";
+                DataReadQF13_WHSUM3_Wh = $"{(QF13Data.WHSUM3_Wh / 1000).ToString("f0")}";
 
-               
-                DataReader4 =  $"QS25 I_LN1 = {Convert.ToString(Math.Round(QS25Data.ILN1_A, 2) )}A";
-                DataReader5 =  $"QS25 I_LN2 = { QS25Data.ILN2_A.ToString("f2")}A";
-                DataReader6 =  $"QS25 I_LN3 = { QS25Data.ILN3_A.ToString("f2")}A";
-              
 
-                DataReader7 = U1 + QS34Data.ULN1_V.ToString("f2") + U;
-                DataReader8 = U2 + QS34Data.ULN2_V.ToString("f2") + U;
-                DataReader9 = U3 + QS34Data.ULN3_V.ToString("f2") + U;
-                                            
                 #endregion
 
             }
             catch (Exception e)
             {
-                DataReader1 = e.ToString();
+                ErrorRead = e.ToString();
             }
         }
     }
